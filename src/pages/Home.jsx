70a8bbace7ac52f589dc9a7d0cc41f49cc3e3276
wmrmweerakoon <wmrmweerakoon@gmail.com>
2026-01-28 16:00:04 +0530

    Fix import errors by removing unused components from Home.jsx and deleting CursorGlow.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,4 @@
 import { Navbar } from "../components/Navbar";
-import { StarBackground } from "@/components/StarBackground";
-import { CursorGlow } from "@/components/CursorGlow";
 import { HeroSection } from "../components/HeroSection";
 import { ProjectsSection } from "../components/ProjectsSection";
 import { ContactSection } from "../components/ContactSection";
@@ -9,10 +7,6 @@ import { Footer } from "../components/Footer";
 export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
-      {/* Background Effects */}
-      <StarBackground />
-      {/* Cursor Glow Effect */}
-      <CursorGlow />
 
       {/* Navbar */}
       <Navbar />
